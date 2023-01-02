3834c8a9cd171e730a3f779c8d5060c1e05f5610
CEOJINSUNG <jinsung1048@gmail.com>
2023-01-02 16:34:31 +0900

    feat : is_chain_valid 구현
    해당 체인이 유효한 체인인지 확인하는 로직

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 use chrono::Utc;
-use serde::{Serialize, Deserialize};
-use sha2::{Sha256, Digest};
-use std::vec::Vec;
+use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
+use std::{error, vec::Vec};
 
 // 블록을 찾는데 해시하기 위해 필요한 데이터 시작점
 const DIFFICULTY_PREFIX: &str = "00";
@@ -40,10 +40,10 @@ pub struct Block {
     pub previous_hash: String,
     pub timestamp: i64,
     pub data: String,
-    pub nonce: u64
+    pub nonce: u64,
 }
 
-impl App{
+impl App {
     fn new() -> Self {
         Self { blocks: vec![] }
     }
@@ -55,7 +55,7 @@ impl App{
             previous_hash: String::from("genesis"),
             data: String::from("genesis"),
             nonce: 0,
-            hash: "0000f816a87f806bb0073dcf026a64fb40c946b5abee2573702828694d5b4c43".to_string()
+            hash: "0000f816a87f806bb0073dcf026a64fb40c946b5abee2573702828694d5b4c43".to_string(),
         };
 
         self.blocks.push(genesis_block);
@@ -63,21 +63,56 @@ impl App{
 
     fn try_add_block(&mut self, block: Block) {
         let latest_block = self.blocks.last().expect("there is at least one block");
-        
+        if self.is_block_valid(&block, latest_block) {
+            self.blocks.push(block);
+        };
     }
 
     // 유효한 블록인지 확인
     fn is_block_valid(&self, block: &Block, previous_block: &Block) -> bool {
         if block.previous_hash != previous_block.hash {
             return false;
-        } else if !hash_to_binary_representation(&hex::decode(&block.hash).expect("can decode from hex")).starts_with(DIFFICULTY_PREFIX) {
+        } else if !hash_to_binary_representation(
+            &hex::decode(&block.hash).expect("can decode from hex"),
+        )
+        .starts_with(DIFFICULTY_PREFIX)
+        {
             return false;
         } else if block.id != previous_block.id + 1 {
             return false;
-        } else if hex::encode(calculate_hash(block.id, block.timestamp, &block.previous_hash, &block.data, block.nonce)) != block.hash {
+        } else if hex::encode(calculate_hash(
+            block.id,
+            block.timestamp,
+            &block.previous_hash,
+            &block.data,
+            block.nonce,
+        )) != block.hash
+        {
             return false;
         }
 
         true
+    }
+
+    fn is_chain_valid(&self, chain: &[Block]) -> bool {
+        for i in 0..chain.len() {
+            if i == 0 {
+                continue;
+            }
+
+            let first = chain.get(i - 1).expect("has to exist");
+            let second = chain.get(i).expect("has to exist");
+            if !self.is_block_valid(second, first) {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    fn choose_chain(&mut self, local: Vec<Block>, remote: Vec<Block>) {
+        let is_local_valid = self.is_chain_valid(&local);
+        let is_remote_valid = self.is_chain_valid(&remote);
+        
     }
 }
