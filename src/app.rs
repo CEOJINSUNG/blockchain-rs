c4408e831172ea6259200321d6e1c191908ecbd3
CEOJINSUNG <jinsung1048@gmail.com>
2023-01-02 15:52:45 +0900

    feat : app, block 선언

---
+++ src/app.rs
@@ -0,0 +1,36 @@
+use chrono::Utc;
+use serde::{Serialize, Deserialize};
+use std::vec::Vec;
+
+pub struct App {
+    pub blocks: Vec<Block>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Block {
+    pub id: u64,
+    pub hash: String,
+    pub previous_hash: String,
+    pub timestamp: i64,
+    pub data: String,
+    pub nonce: u64
+}
+
+impl App{
+    fn new() -> Self {
+        Self { blocks: vec![] }
+    }
+
+    fn genesis(&mut self) {
+        let genesis_block = Block {
+            id: 0,
+            timestamp: Utc::now().timestamp(),
+            previous_hash: String::from("genesis"),
+            data: String::from("genesis"),
+            nonce: 0,
+            hash: "0000f816a87f806bb0073dcf026a64fb40c946b5abee2573702828694d5b4c43".to_string()
+        };
+
+        self.blocks.push(genesis_block);
+    }
+}
